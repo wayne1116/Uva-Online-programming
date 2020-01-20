4d14c0423d2bc99fd4cb7d5b3660b375cfa7fca1
Wayne Chen <wayne861116@gmail.com>
2020-01-21 01:43:57 +0800

    Update and rename JAVA/Main495.java to Java/495-Fibonacci Freeze.java

---
+++ Java/495-Fibonacci Freeze.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main{
+	public static void main(String[] argv){
+        BigInteger[] fib=new BigInteger[5001];
+        Scanner in=new Scanner(System.in);
+        fib[0]=new BigInteger("0");
+        fib[1]=new BigInteger("1");
+		for(int i=2; i<=5000; i++){
+            fib[i]=fib[i-1].add(fib[i-2]);
+		}
+		while(in.hasNextInt()){
+			int num=in.nextInt();
+			System.out.println("The Fibonacci number for "+num+" is "+fib[num]);
+		}
+	}
+}
