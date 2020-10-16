906c5b3d87e782cebff74a084edb3e8c60637a9e
Wayne Chen <wayne861116@gmail.com>
2020-10-17 01:24:27 +0800

    Update 10276.cpp

--- C++/10276.cpp
+++ C++/10276.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 int main()
 {
-	int a, n, count, temp, i;
-	int top[64];
-	cin >> a;
-	while(a--){
-		cin >> n;
-		for(i=0; i<n; i++) top[i]=0;
-		count=1;
-		while(true){
-			for(i=0; i<n; i++){
-				temp=sqrt(top[i]+count);
-				if(temp*temp==top[i]+count||top[i]==0){
-					top[i]=count;
-					break;
-				}
-			}
-			if(i>=n) break;
-			++count;
-		}
-		cout << count-1 << endl;
-	}
-	return 0;
+    int a, n, count, temp, i;
+    int top[64];
+    cin >> a;
+    while(a--){
+        cin >> n;
+	for(i=0; i<n; i++) top[i]=0;
+	count=1;
+	while(true){
+	    for(i=0; i<n; i++){
+		temp=sqrt(top[i]+count);
+		if(temp*temp==top[i]+count||top[i]==0){
+			top[i]=count;
+			break;
+	        }
+	    }
+	    if(i>=n) break;
+	    ++count;
+        }
+        cout << count-1 << endl;
+    }
+    return 0;
 }
